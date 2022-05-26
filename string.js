28c75ecf56cf3dbef54a2d8ae68a2e143c320957
legendbahi <hamzabava70@gmail.com>
2022-05-27 00:56:32 +0300

    string

---
+++ string.js
@@ -0,0 +1,13 @@
+//o find the length of a string, use the built-in length property:
+let x="abcde"
+console.log(x.length)
+
+//The sequence \"  inserts a double quote in a string:
+
+let y="you are my \"broo\""
+console.log(y)
+
+
+//cutting lines
+let z="you are my \  broo"
+console.log(z)
