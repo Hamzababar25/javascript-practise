d710ca4ef3f320ab4ee1aafdff6669c84793ae03
legendbahi <hamzabava70@gmail.com>
2022-06-06 08:54:48 +0300

    number

---
+++ numbermethods.js
@@ -0,0 +1,30 @@
+//The toString() method returns a number as a string.
+
+//All number methods canbe used on any type of numbers (literals, variables, or expressions):
+
+let x = 123;
+x.valueOf();
+(123).valueOf();
+(100 + 23).valueOf();
+
+
+//
+
+//string ko number mein convert krta ha
+var x1="10"
+console.log(Number(x1));
+
+
+var n="22"
+console.log(parseInt(n))
+
+
+
+let x2 = 6;
+x2.MAX_VALUE
+console.log(x2+"hf");
+
+
+///nan
+n1="years 10"
+console.log(parseFloat(n1));
